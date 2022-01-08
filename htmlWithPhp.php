fb25d1338b842ccb6d65394879647fcb5895a8b8
Adarsh Rawatt <87625228+Adarshrawatt@users.noreply.github.com>
2022-01-08 07:28:03 -0800

    Add files via upload

---
+++ htmlWithPhp.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>html and php</title>
+</head>
+<body>
+     <!-- we can also use html and css with the ".php" extension -->
+    <h1> <center>
+        <?php
+        echo"Heading of the website";
+        ?>
+        </center>
+    </h1>
+    <center>
+    <?php
+    echo"this is a body of the website and this text is aling in center";
+    ?>
+      </center>
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,6 @@
+<?php
+// php is the not a case sensitive language but in case of variables it is case sensitive
+$name = "Adarsh";
+$Name = "Rohan";
+eCho "this is text of website<br>";
+?>
