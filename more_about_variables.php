34fb1d8125766cd7ee12559590fe96246fdf2009
Adarsh Rawatt <87625228+Adarshrawatt@users.noreply.github.com>
2022-01-09 08:43:42 -0800

    Add files via upload

--- more_about_variables.php
+++ more_about_variables.php
@@ -1,3 +1,15 @@
 <?php
-echo "hello world";
+$name = "superman";
+echo "$name is very fast and intelligent guy";
+// variables are the containers which helps us to contain various types of data
+// variables in the php ara case-sensitive
+// like there is a quit difference between 
+//     $name    and     $Name 
+// both are the differnt
+
+// rules for the variables in the php
+// 1=> variables are the alphanumerical values only
+// 2=> not start with the number and and special character
+// 3=> started with the underscore and and the character
+// 4=> always started with the "$" this sign
 ?>
