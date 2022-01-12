f46aa4f3abaa2fa49683b9528ba423c4ced8d11c
Adarsh Rawatt <87625228+Adarshrawatt@users.noreply.github.com>
2022-01-12 15:49:21 +0330

    Add files via upload

---
+++ switch_case_statement.php
@@ -0,0 +1,67 @@
+<?php
+
+// it can be of any data-type
+
+// 1> data-type  - int 
+$number = 5;
+switch($number){
+    case 1: echo "your number is one";
+    echo "<br>";
+    break;
+    case 2: echo "your number is two";
+     echo "<br>";
+    break;
+    case 3: echo "your number is three";
+     echo "<br>";
+    break;
+    case 4: echo "your number is four"; 
+    echo "<br>";
+    break;
+    case 5: echo "your number is five"; 
+    echo "<br>";
+    break;
+}
+
+
+// 2> data-type  - string
+$name = "adarsh";
+switch($name){
+    case "adarsh" : echo "i think your name is adarsh rawat";
+     echo "<br>";
+    break;
+    case "rohan" : echo "i think your name is rohan rawat"; 
+    echo "<br>";
+    break;
+    case "rupal" : echo "i think your name is rupal rawat";
+     echo "<br>";
+    break;
+    case "arnav" : echo "i think your name is arnav rawat";
+     echo "<br>";
+    break; 
+    case "ramesh" : echo "i think your name is ramesh rawat"; 
+    echo "<br>";
+    break;
+}
+
+
+
+// 3> data-type  - char
+$alpha = 'b';
+switch($alpha){
+case 'a' : echo "i think your name is adarsh rawat";
+ echo "<br>";
+break;
+case 'b' : echo "i think your name is rohan rawat";
+ echo "<br>";
+break;
+case 'c' : echo "i think your name is rupal rawat"; 
+echo "<br>";
+break;
+case 'd' : echo "i think your name is arnav rawat";
+ echo "<br>";
+break; 
+case 'e' : echo "i think your name is ramesh rawat";
+ echo "<br>";
+break;
+}
+?>
