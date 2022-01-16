649f1dd9c09776895e1788146357836c1f4cb4c4
Adarsh Rawatt <87625228+Adarshrawatt@users.noreply.github.com>
2022-01-16 14:02:49 +0330

    Add files via upload

---
+++ date_function.php
@@ -0,0 +1,7 @@
+<?php
+echo "<h1>Welcome to the date function</h1>";
+$date = date("d S");
+$month = date("M");
+echo "today's date is ".$date;
+echo "today's month is ".$month;
+?>
--- variables.php
+++ variables.php
@@ -2,5 +2,7 @@
 // php is the not a case sensitive language but in case of variables it is case sensitive
 $name = "Adarsh";
 $Name = "Rohan";
-eCho "this is text of website<br>";
+$num = 45;
+echo "this is text of website<br>";
+echo "this is my number : ".$num;
 ?>
